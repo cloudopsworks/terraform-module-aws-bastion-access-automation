4d46e853c4da1b18a54f0c5773a0333be141d2c8
Cristian Beraha <cristian@cloudops.works>
2025-08-27 09:19:38 -0300

    Add EventBridge Scheduler for Bastion host shutdown automation
    - Created `aws_scheduler_schedule` resource to define a cron-based schedule for triggering Lambda to shut down Bastion hosts.
    - Configured schedule expression and timezone.
    - Set up target Lambda function with appropriate input and IAM role.

---
+++ bastion-schedule.tf
@@ -0,0 +1,25 @@
+##
+# (c) 2021-2025
+#     Cloud Ops Works LLC - https://cloudops.works/
+#     Find us on:
+#       GitHub: https://github.com/cloudopsworks
+#       WebSite: https://cloudops.works
+#     Distributed Under Apache v2.0 License
+#
+
+resource "aws_scheduler_schedule" "this" {
+  name        = "${local.function_name_short}-bastion-shutdown"
+  description = "Schedule to trigger Lambda function ${aws_lambda_function.this.function_name} for bastion host shutdown"
+  flexible_time_window {
+    mode = "OFF"
+  }
+  schedule_expression          = "cron(${var.settings.bastion_shutdown.cron})"
+  schedule_expression_timezone = try(var.settings.bastion_shutdown.timezone, null)
+  target {
+    arn      = aws_lambda_function.this.arn
+    role_arn = aws_iam_role.lambda_exec.arn
+    input = jsonencode({
+      action = "shutdown_bastion"
+    })
+  }
+}
