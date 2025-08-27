8824a077eba7aaf8a9ccd5811721967e9e865e64
Cristian Beraha <cristian@cloudops.works>
2025-08-27 13:03:02 -0300

    Add support for dual SQS queues for request and response handling
    - Introduced `response_queue_name` alongside `request_queue_name` in `sqs.tf`.
    - Added new SQS queue resource for response handling with associated configurations.
    - Updated SSM parameters to store URLs for both request and response queues.
    - Expanded outputs to include ARNs and URLs for both queues.

--- sqs.tf
+++ sqs.tf
@@ -8,12 +8,20 @@
 #
 
 locals {
-  sqs_queue_name = "${local.function_name}-sqs-queue"
+  sqs_queue_name = "${local.function_name}-request-queue"
+  response_queue_name = "${local.function_name}-response-queue"
 }
 resource "aws_sqs_queue" "this" {
   name                       = local.sqs_queue_name
   sqs_managed_sse_enabled    = true
-  visibility_timeout_seconds = 90
+  visibility_timeout_seconds = try(var.settings.timeout, 60) + 30
+  tags                       = local.all_tags
+}
+
+resource "aws_sqs_queue" "response" {
+  name                       = local.response_queue_name
+  sqs_managed_sse_enabled    = true
+  visibility_timeout_seconds = try(var.settings.timeout, 60) + 30
   tags                       = local.all_tags
 }
 
@@ -29,9 +37,17 @@ resource "aws_lambda_event_source_mapping" "this" {
 }
 
 resource "aws_ssm_parameter" "sqs_queue_url" {
-  name        = "/cloudopsworks/tronador/access-automation/sqs-queue"
-  description = "Tronador Access Automation SQS Queue URL - ${local.system_name}"
+  name        = "/cloudopsworks/tronador/access-automation/request-queue"
+  description = "Tronador Access Automation SQS Request Queue URL - ${local.system_name}"
   type        = "String"
   value       = aws_sqs_queue.this.url
+  tags        = local.all_tags
+}
+
+resource "aws_ssm_parameter" "sqs_response_queue_url" {
+  name        = "/cloudopsworks/tronador/access-automation/response-queue"
+  description = "Tronador Access Automation SQS Response Queue URL - ${local.system_name}"
+  type        = "String"
+  value       = aws_sqs_queue.response.url
   tags        = local.all_tags
 }
